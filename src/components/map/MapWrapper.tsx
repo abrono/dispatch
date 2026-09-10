7f87e190ab74844b2a187d093587ea23ca5a7fdf
abrono <abonoriode@gmail.com>
2026-09-10 12:40:24 +0100

    Create MapWrapper component for Leaflet map
    Added MapWrapper component to render Leaflet map.

--- src/components/map/MapWrapper.tsx
+++ src/components/map/MapWrapper.tsx
@@ -1,1 +1,11 @@
+import { LeafletMap } from './LeafletMap';
+import type { MapWrapperProps } from './types';
 
+/**
+ * Provider-agnostic map. Today renders Leaflet/OSM.
+ * To swap to Google Maps later: replace the body of this component and
+ * nothing else in the codebase needs to change.
+ */
+export function MapWrapper(props: MapWrapperProps) {
+  return <LeafletMap {...props} />;
+}
