ed5f5a9e82f9153d1d8942609c2ab8faf37c9200
abrono <abonoriode@gmail.com>
2026-09-11 17:44:02 +0100

    Wrap App component with ErrorBoundary and Toaster

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
 import App from './App';
+import { ErrorBoundary } from './components/ErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+      <Toaster position="top-right" />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
